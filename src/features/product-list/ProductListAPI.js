301bc93539b0b2d0b780cb8ccdca01e7878752ed
somcpp <saubhagya.sharma3.14@gmail.com>
2025-06-27 23:17:52 +0530

    added filter and divided in components

--- src/features/product-list/ProductListAPI.js
+++ src/features/product-list/ProductListAPI.js
@@ -1,9 +1,21 @@
 // A mock function to mimic making an async request for data
-export function fetchCount(amount = 1) {
-  return new Promise((resolve) =>{
-    const response = fetch('http://localhost:8080')
-    const data =  response.json
+export function fetchAllProducts(amount = 1) {
+  return new Promise(async(resolve) =>{
+    const response =await fetch('http://localhost:8080/products')
+    const data = await response.json()
     resolve({data}) //d
   }
   );
+}
+export function fetchProductByFilters(filter){
+  let queryString = '';
+  for(let key in filter){
+    queryString += `${key}=${filter[key]}&`
+  }
+
+  return new Promise (async (resolve) => {
+    const response = await fetch('http://localhost:8080/products?'+queryString)
+    const data = await response.json()
+    resolve({data})
+  })
 }
--- src/features/product-list/ProductListSlice.js
+++ src/features/product-list/ProductListSlice.js
@@ -1,29 +1,34 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchCount } from './ProductListAPI';
-
+import { fetchAllProducts } from './ProductListAPI';
+import { fetchProductByFilters } from './ProductListAPI';
 const initialState = {
-  value: 0,
+  products: [],
   status: 'idle',
 };
 
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched. Thunks are
-// typically used to make async requests.
-export const incrementAsync = createAsyncThunk(
-  'counter/fetchCount',
-  async (amount) => {
-    const response = await fetchCount(amount);
+
+export const fetchAllProductsAsync = createAsyncThunk(
+  'product/fetchAllProducts',
+  async () => {
+    const response = await fetchAllProducts();
+    // The value we return becomes the `fulfilled` action payload
+    return response.data;
+  }
+);
+export const fetchProductsByFiltersAsync = createAsyncThunk(
+  'product/fetchProductsByFilters',
+  async (filter) => {
+    const response = await fetchProductByFilters(filter);
     // The value we return becomes the `fulfilled` action payload
     return response.data;
   }
 );
 
-export const counterSlice = createSlice({
-  name: 'counter',
+
+
+export const productSlice = createSlice({
+  name: 'product',
   initialState,
-  // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
     increment: (state) => {
       state.value += 1;
@@ -31,20 +36,27 @@ export const counterSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
-      .addCase(incrementAsync.pending, (state) => {
+      .addCase(fetchAllProductsAsync.pending, (state) => {
         state.status = 'loading';
       })
-      .addCase(incrementAsync.fulfilled, (state, action) => {
+      .addCase(fetchAllProductsAsync.fulfilled, (state, action) => {
         state.status = 'idle';
-        state.value += action.payload;
+        state.products = action.payload;
+      })
+      .addCase(fetchProductsByFiltersAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchProductsByFiltersAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.products = action.payload;
       });
   },
 });
 
-export const { increment } = counterSlice.actions;
+export const { increment } = productSlice.actions;
+
+export const selectAllProducts = (state) => state.product.products;
 
 
-export const selectCount = (state) => state.counter.value;
 
-
-export default counterSlice.reducer;
+export default productSlice.reducer;
